69883a2bbba1e3b2905fb94ccb9656aa31c782c5
Facundo Taddia <facundotaddia35@gmail.com>
2023-01-07 23:24:05 -0300

    Cambio de traduccion de ingles a español

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 let container = document.querySelector(".containerHijo")
 let timeNow = new Date().getHours();
 let greeting = timeNow >= 5 && timeNow < 12
-? "Good morning"
+? "Buenos dias"
 : timeNow >= 12 && timeNow < 18
-? "Good afternoon"
-: "Good evening";
+? "Buenas tardes"
+: "Buenas noches";
 container.innerHTML = `<h1>${greeting}</h1>`;
